df0878e2a66bc7c2c044789892fac64e0b607d42
Bruno PC <work@brunopc.net>
2024-12-13 18:17:41 -0500

    Publishing with gha

--- publish.ps1
+++
@@ -1,6 +0,0 @@
-docker build -t publish-bufferbloat-image .
-docker run -d -e NPM_TOKEN=$env:NPM_PUBLISH_TOKEN --name publish-bufferbloat-container publish-bufferbloat-image
-docker logs -f publish-bufferbloat-container
-docker wait publish-bufferbloat-container > $null 2>&1
-
-pause
